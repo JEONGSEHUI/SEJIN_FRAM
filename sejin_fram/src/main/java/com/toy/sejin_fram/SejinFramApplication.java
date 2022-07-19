5367971cf421118ca5a06f2abed74d54446bc3e8
zarta <tpgml3285@i-mobilepark.com>
2022-07-19 10:42:16 +0900

    init

---
+++ sejin_fram/src/main/java/com/toy/sejin_fram/SejinFramApplication.java
@@ -0,0 +1,13 @@
+package com.toy.sejin_fram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejinFramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejinFramApplication.class, args);
+	}
+
+}
---
+++ sejin_fram/src/test/java/com/toy/sejin_fram/SejinFramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.sejin_fram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejinFramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
