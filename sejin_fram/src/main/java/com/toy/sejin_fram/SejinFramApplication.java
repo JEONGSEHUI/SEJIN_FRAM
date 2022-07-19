f1d38345a66a3b4f9cec8b21d25ababe5eb850b1
zarta <tpgml3285@i-mobilepark.com>
2022-07-19 11:28:41 +0900

    controller 생성, html 추가

--- sejin_fram/src/main/java/com/toy/sejin_fram/SejinFramApplication.java
+++ sejin_fram/src/main/java/com/toy/sejin_fram/SejinFramApplication.java
@@ -3,9 +3,16 @@ package com.toy.sejin_fram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SejinFramApplication {
+
+	@RequestMapping(value="/")
+	String home(){
+	
+		return "redirect:/index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SejinFramApplication.class, args);
