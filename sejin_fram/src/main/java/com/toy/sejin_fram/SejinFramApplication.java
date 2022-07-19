d6f3e28df1e4eb8b6073e42c55578fca5f49b739
zarta <tpgml3285@i-mobilepark.com>
2022-07-19 11:05:48 +0900

    두번째 커밋

--- sejin_fram/src/main/java/com/toy/sejin_fram/SejinFramApplication.java
+++ sejin_fram/src/main/java/com/toy/sejin_fram/SejinFramApplication.java
@@ -2,8 +2,9 @@ package com.toy.sejin_fram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SejinFramApplication {
 
 	public static void main(String[] args) {
